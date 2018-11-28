fbdf3287855a907e540b15c693d49e5e7ceb7335
EB-Elder <44158678+EB-Elder@users.noreply.github.com>
2018-11-28 20:35:28 +0100

    Add files via upload
    Ne pas oubliez d'assigné le GameObject crée à la position du joueur a ce script

--- AnnualProject_ABD/Assets/Script/ThirdPersonController.cs
+++ AnnualProject_ABD/Assets/Script/ThirdPersonController.cs
@@ -5,41 +5,50 @@ using UnityEngine;
 public class ThirdPersonController : MonoBehaviour {
 
     public AttractorScript attractor;
+
+    [SerializeField]
+    private GameObject cameraRotatorX;
+
     private Vector3 moveDir;
     private Vector3 jumpDir;
     public float speed;
+
     public Rigidbody rb;
+
+    [SerializeField]
+    private float rotationSpeed = 5.0f;
+
     private bool isJumping = false;
+
     public GameObject orb;
     public GameObject orbSpawned;
 
-    void Start()
+    void Start ()
     {
-        orbSpawned = Instantiate(orb, transform);
-        orbSpawned.transform.position = transform.position + new Vector3(3, 0);
+        cameraRotatorX.transform.rotation = transform.rotation;
     }
 
-    // Use this for initialization
-    void Update () {
-        orbSpawned.transform.RotateAround(transform.position, Vector3.up,40 * Time.deltaTime);
-        
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    void FixedUpdate () {
+        //Gestion des déplacements autour de la planète
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         rb.MovePosition(rb.position + transform.TransformDirection(moveDir) * speed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotationSpeed, 0));
+        cameraRotatorX.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * rotationSpeed, 0, 0), Space.Self);
+
+        //Gestion du saut en appliquant force plus èlevé que la gravité.
         if(Input.GetKey(KeyCode.Space) && !isJumping)
         {
             jumpDir = attractor.dirForce;
             rb.AddForce(jumpDir * 250);
+
+        //Interdiction du saut
             isJumping = true;
         }
     }
-
+    //Autorisation du saut
     void OnCollisionEnter(Collision hit)
     {
-        if(hit.gameObject.tag == "floor")
+        if(hit.gameObject.tag == "planet")
         {
             isJumping = false;
         }
