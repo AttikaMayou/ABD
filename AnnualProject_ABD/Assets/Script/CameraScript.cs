43dce8c30e5c1f18bf41814931d64cd0287c4422
EB-Elder <44158678+EB-Elder@users.noreply.github.com>
2018-11-28 20:34:37 +0100

    Script de Camera
    Ne pas oubliez de crée un GameObject vide à la position du joueur et de l'assigné a ce script

---
+++ AnnualProject_ABD/Assets/Script/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public string playerTag = "Player";
+    private Vector3 cameraOffset;
+
+    [SerializeField]
+    private GameObject cameraRotatorX;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Récupération de la position du joueur au démarrage
+        Transform player = GameObject.FindGameObjectWithTag(playerTag).transform;
+        //cameraOffset = Distance entre la caméra et le joueur
+        cameraOffset = player.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Récupération de la position du joueur à chaque frame
+        Transform player = GameObject.FindGameObjectWithTag(playerTag).transform;
+        //Permet de tourner autour du perso
+
+        transform.position = player.position;
+        transform.rotation = player.rotation * cameraRotatorX.transform.rotation;
+        transform.position -= transform.rotation * cameraOffset;
+        
+    }
+}
