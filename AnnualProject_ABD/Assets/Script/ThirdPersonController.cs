dbf837e9b3a0c0a61ace80c20219a020f04524e0
EB-Elder <44158678+EB-Elder@users.noreply.github.com>
2018-12-01 21:23:34 +0100

    Add files via upload
    Ajout du blocage de la camera

--- AnnualProject_ABD/Assets/Script/ThirdPersonController.cs
+++ AnnualProject_ABD/Assets/Script/ThirdPersonController.cs
@@ -22,19 +22,31 @@ public class ThirdPersonController : MonoBehaviour {
 
     public GameObject orb;
     public GameObject orbSpawned;
+    
 
     void Start ()
     {
         cameraRotatorX.transform.rotation = transform.rotation;
     }
-
+   
     void FixedUpdate () {
         //Gestion des déplacements autour de la planète
         moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         rb.MovePosition(rb.position + transform.TransformDirection(moveDir) * speed * Time.deltaTime);
+        //Permet de tourner le personnage pour donner la direction à la caméra sur l'axe horizontale
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotationSpeed, 0));
-        cameraRotatorX.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * rotationSpeed, 0, 0), Space.Self);
-
+        //Permet de tourner le gameobject qui donne la direction à la caméra sur l'axe vertical
+        cameraRotatorX.transform.Rotate(new Vector3(-(Input.GetAxis("Mouse Y") * rotationSpeed), 0, 0), Space.Self);
+        //Les deux conditions juste en dessous permettent à la caméra de ne pas aller trop haut ni trop bas
+        //Les valeurs qui était mise sont des valeurs qui peuvent être pris par la variable cameraRotatorX.transform.rotation.x (-1 - 1)
+        if (cameraRotatorX.transform.rotation.x > 0.42f)
+        {
+            cameraRotatorX.transform.rotation = new Quaternion(0.42f, cameraRotatorX.transform.rotation.y, cameraRotatorX.transform.rotation.z, cameraRotatorX.transform.rotation.w);
+        }
+        if (cameraRotatorX.transform.rotation.x < -0.2f)
+        {
+            cameraRotatorX.transform.rotation = new Quaternion(-0.2f, cameraRotatorX.transform.rotation.y, cameraRotatorX.transform.rotation.z, cameraRotatorX.transform.rotation.w);
+        }
         //Gestion du saut en appliquant force plus èlevé que la gravité.
         if(Input.GetKey(KeyCode.Space) && !isJumping)
         {
