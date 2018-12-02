69f8a0555c48810d7de144f036db504c27403893
Abdallah <elhajjam.abdallah1@gmail.com>
2018-12-02 18:36:59 +0100

    Création du ficher "Resources" & Ajout d'un prefab de test dans le fichier "Resources" & Création d'un proto de script de Sort

---
+++ AnnualProject_ABD/Assets/Script/spellThrower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spellThrower : MonoBehaviour {
+
+    private GameObject testOrb;
+    private bool canThrowSpell;
+
+	// Use this for initialization
+	void Start () {
+        testOrb = Resources.Load<GameObject>("testOrb");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A) && canThrowSpell)
+        {
+            canThrowSpell = false;
+            Instantiate(testOrb, transform.position + new Vector3(transform.forward.x, transform.forward.y, transform.forward.z + 3), transform.rotation);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            canThrowSpell = true;
+        }
+    }
+}
