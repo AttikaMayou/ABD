2cd6ff57e689f87d310c66a64f0eabe9e8b99cd3
Margot <gotgot07@hotmail.fr>
2018-12-02 17:04:37 +0100

    Animation : à revoir = jump

---
+++ AnnualProject_ABD/Assets/Script/Animation/AnimationCharacter/PlayerMovements.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    [Tooltip("Player")]
+    private CharacterController Character;
+
+    [SerializeField]
+    [Tooltip("Vitesse de la marche")]
+    private float moveSpeed=3f;
+
+    [SerializeField]
+    [Tooltip("Vitesse de la course")]
+    private float runSpeed;
+
+    [SerializeField]
+    [Tooltip("Vitesse de la course")]
+    private float WalkSpeed;
+
+    private Animator anim;
+    private bool IsWalking;
+    private bool IsRunning;
+
+
+    // Deux floats pour faire avancer : Horizontal = walk devant derriere / Vertical = walk gauche droite
+    private float Forward;
+    private float Strafe;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Character = GetComponent<CharacterController>();
+        WalkSpeed = moveSpeed * 100;
+        runSpeed = runSpeed * 100;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Récupération des floats vertical et horizontal de l'animator au script
+        Forward = Input.GetAxis("Vertical");
+        Strafe = Input.GetAxis("Horizontal");
+
+        var movement = new Vector3(Strafe, 0, Forward);
+
+        //Running
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Forward!=0)
+            {
+                anim.SetBool("IsRunning", true);
+                anim.SetBool("IsWalking", false);
+                moveSpeed = runSpeed;
+            }
+        }
+        else
+        {
+            //Walking
+            if(Forward!=0)
+            {
+                anim.SetBool("IsRunning", false);
+                anim.SetBool("IsWalking", true);
+                moveSpeed = WalkSpeed;
+            }
+            //Iddle
+            if(Forward==0)
+            {
+                anim.SetBool("IsRunning", false);
+                anim.SetBool("IsWalking", false);
+                moveSpeed = 0;
+            }
+        }
+
+        //* delta time pour que tous les joueurs est le même speed
+        Character.SimpleMove(movement * Time.deltaTime * moveSpeed);
+        anim.SetFloat("Forward", movement.magnitude);
+
+           
+
+    }
+
+
+
+}
+
--- AnnualProject_ABD/Assets/Script/Animation/AnimationCharacter/TestAnimation.cs
+++ AnnualProject_ABD/Assets/Script/Animation/AnimationCharacter/TestAnimation.cs
@@ -15,8 +15,11 @@ public class TestAnimation : MonoBehaviour {
     [SerializeField]
     [Tooltip("Animator du personnage")]
     private Animator anim;
+    
+    // Deux floats pour faire avancer : Horizontal = walk devant derriere / Vertical = walk gauche droite
+    private float animVertical;
+    private float animHorizontal;
 
-    private bool IsWalking=false;
 
     // Use this for initialization
     void Start ()
@@ -27,12 +30,21 @@ public class TestAnimation : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        // Récupération des floats vertical et horizontal de l'animator au script
+        animHorizontal = Input.GetAxis("Horizontal");
+        animVertical = Input.GetAxis("Vertical");
+        anim.SetFloat("animVertical", animVertical);
+        anim.SetFloat("animHorizontal", animHorizontal);
+
+
+
         //reculer
         if (Input.GetKey(KeyCode.S))
         {
             targetTransform.position += Vector3.back * Time.deltaTime * moveSpeed;
         }
         //avancer
+      
         if (Input.GetKey(KeyCode.Z))
         {
             targetTransform.position += Vector3.forward * Time.deltaTime * moveSpeed;
@@ -42,17 +54,16 @@ public class TestAnimation : MonoBehaviour {
         if (Input.GetKey(KeyCode.Q))
         {
             targetTransform.position += Vector3.left * Time.deltaTime * moveSpeed;
-            IsWalking = true;
         }
         //droite
         if (Input.GetKey(KeyCode.D))
         {
             targetTransform.position += Vector3.right * Time.deltaTime * moveSpeed;
-            IsWalking = true;
         }
         //se baisser ++
 
 
         //sauter
+
     }
 }
