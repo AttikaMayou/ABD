74bb21d17cc6dbc31f0617b88fa9a0193fbb16ee
Attika <attika.mayou@gmail.com>
2018-12-05 08:22:54 +0100

    WIP Photon Scripting
    Ajout d'un dossier 'Photon' dans 'Script', ainsi qu'un script

---
+++ AnnualProject_ABD/Assets/Script/Photon/NetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class NetworkManager : MonoBehaviour {
+
+    [FormerlySerializedAs("PlayerPrefab")] [SerializeField] private GameObject _playerPrefab;
+    [FormerlySerializedAs("SpawnPoint")] [SerializeField] private GameObject _spawnPoint;
+
+    private void Start () 
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void OnJoinedLobby()
+    {
+        var myRoomOptions = new RoomOptions {MaxPlayers = 4};
+        //PhotonNetwork.playerName = "Player" + Random.Range(1, 500);
+        PhotonNetwork.JoinOrCreateRoom("The Game", myRoomOptions, TypedLobby.Default);
+    }
+
+    private void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(_playerPrefab.name, _spawnPoint.transform.position, Quaternion.identity, 0);
+    }
+}
